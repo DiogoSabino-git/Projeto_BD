6c4506ca37e98f3ac6975b4827f9d8894451dbe9
inesspp242 <202300242@estudantes.ips.pt>
2025-06-11 14:43:06 +0100

    Triggers

--- test_triggers.sql
+++ test_triggers.sql
@@ -23,35 +23,7 @@ END;
 
 DELIMITER ;
 
-DELIMITER $$
 
-CREATE TRIGGER valida_datas_leilao BEFORE INSERT ON leiloes
-FOR EACH ROW
-BEGIN
-    IF NEW.data_fim IS NOT NULL AND NEW.data_inicio >= NEW.data_fim THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Erro: data_inicio deve ser antes da data_fim.';
-    END IF;
-END $$
-
-DELIMITER ;
-
-DELIMITER $$
-
-CREATE TRIGGER valida_valor_lance BEFORE INSERT ON licitacoes
-FOR EACH ROW
-BEGIN
-    DECLARE maior_valor DECIMAL(10,2);
-
-    SELECT MAX(valor_l) INTO maior_valor
-    FROM licitacoes
-    WHERE id_artigo = NEW.id_artigo;
-
-    IF maior_valor IS NOT NULL AND NEW.valor_l <= maior_valor THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Erro: O novo lance deve ser maior que o maior lance anterior.';
-    END IF;
-END $$
-
-DELIMITER ;
 
 
 
