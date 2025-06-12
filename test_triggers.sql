9114c3ec163f7d1b5147164fc62410911f20a421
DiogoSabino-git <202300149@estudantes.ips.pt>
2025-06-12 14:52:01 +0100

    triggers

--- test_triggers.sql
+++ test_triggers.sql
@@ -23,6 +23,38 @@ END;
 
 DELIMITER ;
 
+DELIMITER $$
+
+CREATE TRIGGER result_change
+AFTER INSERT ON resultados
+FOR EACH ROW
+BEGIN
+    INSERT INTO tbl_logs (event_type, id_participante, id_leilao, new_value)
+    VALUES ('INSERT', NEW.id_participante, NEW.id_leilao, NEW.valor_final);
+END$$
+
+CREATE TRIGGER result_update_change
+AFTER UPDATE ON resultados
+FOR EACH ROW
+BEGIN
+    INSERT INTO tbl_logs (event_type, id_participante, id_leilao, old_value, new_value)
+    VALUES ('UPDATE', NEW.id_participante, NEW.id_leilao, OLD.valor_final, NEW.valor_final);
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER result_delete_log
+AFTER DELETE ON resultados
+FOR EACH ROW
+BEGIN
+    INSERT INTO tbl_logs (event_type, id_participante, id_leilao, old_value)
+    VALUES ('DELETE', OLD.id_participante, OLD.id_leilao, OLD.valor_final);
+END$$
+
+DELIMITER ;
+
 
 
 
